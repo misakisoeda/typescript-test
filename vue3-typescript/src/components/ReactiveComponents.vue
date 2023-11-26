aef57af620f104e9de6ba5aa076d02df70437eda
misakisoeda <misaki.soeda.29@gmail.com>
2023-11-26 17:22:40 +0900

    feat: vue3

---
+++ vue3-typescript/src/components/ReactiveComponents.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+type User = {
+    id: number,
+    name: string
+}
+const user1: User = {
+    id: 1,
+    name: '添田'
+}
+
+const user2 = reactive({} as User)
+user2.id = 1
+user2.name = '伏黒'
+
+
+</script>
+
+<template>
+    <article>
+        <h2>reacrive</h2>
+        {{ user1.name }}
+        {{ user2.name }}
+    </article>
+</template>
